4e197d35d4f55b0c4ba7c1bb1e72b775ceb2afdf
Ryan Lucchese <ryan.lucchese@write-c.com>
2017-08-09 11:28:48 -0600

    prototyping C API

--- egihash.h
+++ egihash.h
@@ -4,13 +4,38 @@
 
 #pragma once
 
-#ifdef __cplusplus
+#include <stdint.h>
 
+#ifdef __cplusplus
 namespace egihash
 {
 	bool test_function();
 }
 
-#else
-// TODO: C API
-#endif
+extern "C"
+{
+#endif // __cplusplus
+
+// TODO: implement C API
+#define EGIHASH_NAMESPACE_PREFIX egihash
+#define EGIHASH_NAMESPACE(name) EGIHASH_NAMESPACE_PREFIX ## _ ## name
+
+typedef int (* EGIHASH_NAMESPACE(callback))(unsigned int);
+typedef struct EGIHASH_NAMESPACE(light) * EGIHASH_NAMESPACE(light_t);
+typedef struct EGIHASH_NAMESPACE(full) * EGIHASH_NAMESPACE(full_t);
+typedef struct EGIHASH_NAMESPACE(h256) { uint8_t b[32]; } EGIHASH_NAMESPACE(h256_t);
+typedef struct EGIHASH_NAMESPACE(result) { EGIHASH_NAMESPACE(h256_t) value; EGIHASH_NAMESPACE(h256_t) mixhash; } EGIHASH_NAMESPACE(result_t);
+
+EGIHASH_NAMESPACE(light_t) EGIHASH_NAMESPACE(light_new)(unsigned int number);
+EGIHASH_NAMESPACE(result_t) EGIHASH_NAMESPACE(light_compute)(EGIHASH_NAMESPACE(light_t) light, EGIHASH_NAMESPACE(h256_t) header_hash, uint64_t nonce);
+void EGIHASH_NAMESPACE(light_delete)(EGIHASH_NAMESPACE(light_t) light);
+
+EGIHASH_NAMESPACE(full_t) EGIHASH_NAMESPACE(full_new)(EGIHASH_NAMESPACE(light_t) light, EGIHASH_NAMESPACE(callback) callback);
+uint64_t EGIHASH_NAMESPACE(full_dag_size)(EGIHASH_NAMESPACE(full_t) full);
+void * const EGIHASH_NAMESPACE(full_dag)(EGIHASH_NAMESPACE(full_t) full);
+EGIHASH_NAMESPACE(result_t) EGIHASH_NAMESPACE(full_compute)(EGIHASH_NAMESPACE(full_t) full, EGIHASH_NAMESPACE(h256_t) header_hash, uint64_t nonce);
+void EGIHASH_NAMESPACE(full_delete)(EGIHASH_NAMESPACE(full_t) full);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif // __cplusplus
